a7dbbc121518e240525f7d048649ff37e9d426b2
enfp-y <dusghk0628@gmail.com>
2023-08-25 16:48:05 +0900

    20230825

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: `enfp-y.github.io`,
-  description: `잡학다식 공부 기록`,
+  description: `Cloud sales 공부 기록`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
   siteUrl: `https://enfp-y.github.io/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
@@ -99,15 +99,15 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: '롯데모바일상품권 프로젝트',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
+          '2013년 첫 사회생활로, 롯데카드에서 신규로 출시한 롯데모바일상품권과 함께 운영/기획 업무를 맡게되었습니다. 소셜커머스, 오픈마켓 등 다양한 판매 채널 확대, 총 100억 매출 달성. 면세점 전용 모바일 상품권 프로젝트 PL. 모바일 상품권 시스템 및 admin 기능 기획. 프로젝트 진척 및 인력 관리'
+        techStack: ['operation', 'abc'],
         thumbnailUrl: 'blog.png',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: '',
+          github: '',
+          demo: '',
         },
       },
     ],
